31586630ba381fdeea5cf8d99c05df88a17569e6
Elvin Thudugala <elvin@outlook.co.nz>
2019-07-17 11:08:24 +1200

    Separating GlyphList in Pro, so that it will work with VS 2017

---
+++ sample/Direct/Sample/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Plugin.Glypher;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //If you want to change Font Name
+            //ChangeFontName();
+
+            //GlyphRegister.Current.Init(typeof(Plugin.Glypher.FontAwesome5Pro.GlyphListBrand));
+            //GlyphRegister.Current.Init(typeof(Plugin.Glypher.FontAwesome5Pro.GlyphListLight));
+            //GlyphRegister.Current.Init(typeof(Plugin.Glypher.FontAwesome5Pro.GlyphListRegular));
+            //GlyphRegister.Current.Init(typeof(Plugin.Glypher.FontAwesome5Pro.GlyphListSolid));
+            GlyphRegister.Current.Init(typeof(Plugin.Glypher.FontAwesome5Free.GlyphList));
+            GlyphRegister.Current.Init(typeof(Plugin.Glypher.WeatherIcons.GlyphList));
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        /// <summary>
+        /// If you want to change Font Name
+        /// </summary>
+        private void ChangeFontName()
+        {
+            var glyphFont = Plugin.Glypher.FontAwesome5Free.GlyphFont.Current;
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    glyphFont.Brand = "fontAwesomeBrands.ttf#Font Awesome 5 Brands Regular";
+                    glyphFont.Regular = "fontAwesomeRegular.ttf#Font Awesome 5 Free Regular";
+                    glyphFont.Solid = "fontAwesomeSolid.ttf#Font Awesome 5 Free Solid";
+                    break;
+            };
+        }
+    }
+}
---
+++ sample/Direct/Sample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Glypher;
+using Plugin.Glypher.FontAwesome5Free;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Redo_Tapped(object sender, EventArgs e)
+        {
+            var toolbarItemLabelColor = Device.RuntimePlatform == Device.Android ? Color.White : Color.Black;
+
+            ToolbarItemLabel.TextColor = ToolbarItemLabel.TextColor == toolbarItemLabelColor ? Color.GreenYellow : toolbarItemLabelColor;
+
+            var oldValueLabel1 = FontGlyph.GetGlyph(Label1);
+            FontGlyph.SetGlyph(Label1, oldValueLabel1 == GlyphList.Fab_Bluetooth ? GlyphList.Fab_Adobe : GlyphList.Fab_Bluetooth);
+
+            var oldValueLabel2 = FontGlyph.GetGlyph(Label2);
+            FontGlyph.SetGlyph(Label2, oldValueLabel2 == GlyphList.Far_Angry ? GlyphList.Far_Address_Book : GlyphList.Far_Angry);
+        }
+    }
+}
